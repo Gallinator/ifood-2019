c785aeab4b5aa142f762916b8e743a9d3a2fa23d
Gallinator <lcgallinator4@gmail.com>
2024-08-04 14:23:33 +0200

    FoodDataLoader: initial commit

---
+++ food_data_loader.py
@@ -0,0 +1,13 @@
+from typing import Optional, Callable
+
+from torchvision.datasets import ImageFolder
+
+
+class FoodDataLoader(ImageFolder):
+    """
+    Simple ImageFolder dataloader which provides an additional dict to convert from class index to label
+    """
+
+    def __init__(self, root: str, transform: Optional[Callable] = None):
+        super().__init__(root, transform=transform)
+        self.idx_to_class = {v: k for k, v in self.class_to_idx.items()}
