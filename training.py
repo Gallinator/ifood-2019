a3c6c42fb0d88e8dd518e6c6fa6cfe58e2914432
Gallinator <lcgallinator4@gmail.com>
2024-08-18 19:07:23 +0200

    training: invert full training order

--- training.py
+++ training.py
@@ -74,8 +74,8 @@ if __name__ == '__main__':
     args = build_arg_parser().parse_args()
     match args.type:
         case 'full':
-            train(args.train_dir, args.val_dir, args.weights_dir)
             ssl_train(get_ssl_data_dir(args.train_dir), get_ssl_data_dir(args.val_dir), args.weights_dir)
+            train(args.train_dir, args.val_dir, args.weights_dir, args.use_ssl_pretrained)
         case 'sup':
             train(args.train_dir, args.val_dir, args.weights_dir)
         case 'selfsup':
