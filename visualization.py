1576de38fd01e245a7b412522cc659aaad5b0be6
Gallinator <lcgallinator4@gmail.com>
2024-08-24 12:31:52 +0200

    visualization: add method tp plot counts

--- visualization.py
+++ visualization.py
@@ -1,4 +1,5 @@
 import PIL.Image
+import numpy as np
 from matplotlib import pyplot as plt
 
 
@@ -7,4 +8,15 @@ def plot_image(image: PIL.Image.Image, label: str):
     plt.title(label.replace('_', ' ').capitalize(), fontsize='xx-large')
     plt.axis('off')
     plt.tight_layout()
+    plt.show()
+
+
+def plot_counts(counts, classes):
+    idx = np.flip(np.argsort(counts))
+    counts = np.array(counts)[idx]
+    classes = np.array(classes)[idx]
+    x = np.arange(len(counts))
+    plt.bar(x, counts, color='salmon')
+    plt.title('Samples count')
+    plt.xticks(x, classes, rotation=90)
     plt.show()
