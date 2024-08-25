6f0143fdd188d324ea85a427480e50d03d2e6356
Gallinator <lcgallinator4@gmail.com>
2024-08-25 16:17:12 +0200

    transforms: remove cut_tiles function

--- transforms.py
+++ transforms.py
@@ -65,21 +65,3 @@ SSL_DATA_TRANSFORM = v2.Compose([v2.Resize(256),
                                  ShuffleJigSaw([[0, 1, 2, 3, 4, 5, 6, 7, 8], [0, 1, 2, 5, 6, 7, 8, 4, 3],
                                                 [0, 3, 2, 8, 6, 7, 1, 4, 5]]),
                                  MultiImageTransform(64, SSL_PER_TILE_TRANSFORM)])
-
-
-def cut_tiles(grid_size: int, inpt: torch.Tensor) -> torch.Tensor:
-    """
-    Given an input tensor cuts tiles.
-    If the image is made of the tiles:
-    >>>[[0,1],
-    >>> [2,3]]
-    Returns the tiles in the order
-    >>>[0,1,2,3]
-    :param grid_size: size of the cut grid. grid_size**2 tiles will be cut.
-    :param inpt: source tensor
-    :return: a list of tiles ordered row wise from left to right
-    """
-    tiles = torch.stack(list(inpt.tensor_split(grid_size, dim=2)), 0)
-    tiles = torch.stack(list(tiles.tensor_split(grid_size, dim=2)), 0)
-    tiles = tiles.flatten(start_dim=0, end_dim=1)
-    return tiles
